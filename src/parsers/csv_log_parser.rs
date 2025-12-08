365db9361d573d1265a45e3421515fadc73b1b19
Gints Polis <polis.gints@gmail.com>
2025-12-08 15:32:40 +0200

    temp

--- src/parsers/csv_log_parser.rs
+++ src/parsers/csv_log_parser.rs
@@ -3,36 +3,82 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-use crate::{convert_args::FileWithPath, parsers::LogParser};
+use chrono::{DateTime, Local};
+use csv::ReaderBuilder;
+use log::error;
+
+use crate::{
+    convert_args::FileWithPath,
+    errors::{PostgresLog, Severity},
+    parsers::{LogLine, LogParser},
+};
 
 pub struct CsvLogParser {
-    reader: BufReader<File>,
+    csv_reader: csv::Reader<BufReader<File>>,
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
 pub type Error = Box<dyn std::error::Error>;
 
-impl CsvLogParser {
+impl<'a> CsvLogParser {
     pub fn new(file_with_path: FileWithPath) -> Self {
-        Self {
-            reader: BufReader::new(file_with_path.file),
-        }
+        let reader = BufReader::new(file_with_path.file);
+        let csv_reader = ReaderBuilder::new()
+            .has_headers(false)
+            .flexible(true) // Allow variable number of columns
+            .from_reader(reader);
+
+        Self { csv_reader }
     }
 }
 
 impl LogParser for CsvLogParser {
-    type Iter = Box<dyn Iterator<Item = Result<String>>>;
+    type Iter = Box<dyn Iterator<Item = Result<LogLine>>>;
 
-    fn parse(self, mask: String) -> Self::Iter {
-        let iter = self.reader.lines().filter_map(move |lin| {
-            let line = match lin {
+    fn parse(
+        mut self,
+        min_severity: i32,
+        mask: Option<String>,
+        begin: Option<DateTime<Local>>,
+        end: Option<DateTime<Local>>,
+    ) -> Self::Iter {
+        let iter = self.csv_reader.records().filter_map(move |rec| {
+            let record = match rec {
                 Ok(l) => l,
                 Err(err) => return Some(Err(format!("Failed to read! Err: {err}").into())),
             };
-            if !line.contains(&mask) {
+            let level: Severity = record[11].to_string().into();
+            let log_level_num: i32 = (&level).into();
+            if log_level_num < min_severity {
                 return None;
+            }
+            if let Some(some_mask) = &mask {
+                if !record[11].starts_with(some_mask) {
+                    return None;
+                };
+            }
+            let log_record: PostgresLog = match record.deserialize(None) {
+                Ok(rec) => rec,
+                Err(e) => {
+                    error!("Error deserializing CSV record in file. {}", e);
+                    return None;
+                }
             };
-            Some(Ok(line))
+            if let Some(log_time) = log_record.log_time {
+                let log_time_local = log_time.with_timezone(&chrono::Local);
+                if let Some(begin) = begin {
+                    if log_time_local < begin {
+                        return None;
+                    }
+                }
+                if let Some(end) = end {
+                    if log_time_local > end {
+                        return None;
+                    }
+                }
+            }
+            
+            Some(Ok(LogLine { pg_log: Some(log_record), raw: "".to_string() }))
         });
         Box::new(iter)
     }
@@ -42,7 +88,7 @@ impl LogParser for CsvLogParser {
 mod tests {
     use std::{fs::File, path::PathBuf};
 
-    use crate::parsers::csv_log_parser::CsvLogParser;
+    use crate::{errors::Severity, parsers::csv_log_parser::CsvLogParser};
 
     use super::*;
 
@@ -52,10 +98,16 @@ mod tests {
         let file = File::open(path.clone())?;
         let parser = CsvLogParser::new(FileWithPath { file, path });
 
-        let iter = parser.parse("2025-05-21 13:00:03.127".to_string());
+        let intseverity = (&(Severity::LOG)).into();
+        let iter = parser.parse(
+            intseverity,
+            Some("2025-05-21 13:00:03.127".to_string()),
+            None,
+            None,
+        );
         for line in iter {
             let line = line?;
-            println!("{}", line);
+            println!("{:?}", line);
         }
 
         Ok(())
--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,17 +1,22 @@
+use chrono::{DateTime, Local};
+
+use crate::errors::PostgresLog;
+
 mod csv_log_parser;
+mod log_log_parser;
 
 pub type Result<T> = core::result::Result<T, Error>;
 pub type Error = Box<dyn std::error::Error>;
 
-// #[derive(Debug)]
-// pub struct LogLine {
-//     pub severity: Severity,
-//     pub raw: String,
-// }
+#[derive(Debug)]
+pub struct LogLine {
+    pub pg_log: Option<PostgresLog>,
+    pub raw: String,
+}
 
 /// Trait for all parsers: produce an iterator over filtered log lines.
 pub trait LogParser {
-    type Iter: Iterator<Item = Result<String>>;
+    type Iter: Iterator<Item = Result<LogLine>>;
 
-    fn parse(self, mask: String) -> Self::Iter;
+    fn parse(self, min_severity: i32, mask: Option<String>, begin: Option<DateTime<Local>>, end: Option<DateTime<Local>>) -> Self::Iter;
 }
